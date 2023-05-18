e6c455d2f822798d231be1e9491c7cb326d66ae3
이승훈 <seunghun@iseunghun-ui-MacBookPro.local>
2023-05-18 17:42:23 +0900

    소스 수정

--- index.ts
+++ index.ts
@@ -1,13 +1,63 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
-import * as _ from 'lodash';
+import * as mysql from 'mysql';
+
+const host = "db-petnmatt.cs0nb5zlvm5n.ap-northeast-2.rds.amazonaws.com";
+const user = "admin";
+const password = "wnakf0510#";
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-    const max = 999;
-    const val = _.random(max);
+    console.log("Event", event);
 
-    const sendResponse = {
-        statusCode: 200,
-        body: `The random value (max ${max}) is: ${val}`,
+    interface Response {
+        statusCode: number;
+        message: string;
+    }
+
+    function sendResponse(statusCode: number, message: string): Response {
+        return {
+            statusCode: statusCode,
+            message: message,
+        };
+    }
+
+    console.log("event", event);
+
+    let tableName = 'users_test';
+    if(!event.body){
+        console.log("Not exist body");
+        return sendResponse(400, 'body is required.');
     };
-    return sendResponse;
+
+    const { body, headers, queryStringParameters } = event;
+    console.log("headers", headers);
+    console.log("queryStringParameters", queryStringParameters);
+    console.log("body", body);
+
+    const requestBody = JSON.parse(body);
+
+    let connection, dbConnection;
+    try{
+        connection = await mysql.createConnection({
+            host     : host,
+            user     : user,
+            password : password,
+            port     : 3306
+        });
+
+        dbConnection = await connection.connect();
+    }catch (e) {
+        console.log("Error in db connection", e);
+        return sendResponse(400, 'connection error');
+    }
+
+    let rows, fields;
+    try{
+        [rows, fields] = await dbConnection.query(`SELECT COUNT(*) FROM ${tableName} WHERE id = ${requestBody.id}`);
+        console.log("rows, fields", rows, fields)
+    }catch (e) {
+        console.log("Error in db query", e);
+        return sendResponse(400, 'query error');
+    };
+
+    return sendResponse(200, 'success');
 };
