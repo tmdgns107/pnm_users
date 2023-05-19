1e9924a6132fcfedf392769a0d2eca3a9821c12a
SeungHun <hooon107@naver.com>
2023-05-19 18:02:44 +0900

    Update util.ts
    mysql -> mysql2 로직변경에 따른 주석 삭제

--- util.ts
+++ util.ts
@@ -1,15 +1,3 @@
-// export function queryMySQL(connection: any, query: string, values: any): Promise<any> {
-//     return new Promise((resolve, reject) => {
-//         connection.query(query, values, (error, results) => {
-//             if (error) {
-//                 reject(error);
-//             } else {
-//                 resolve(results);
-//             }
-//         });
-//     });
-// }
-
 export async function queryMySQL(connection: any, query: string, values: any): Promise<any> {
     try {
         const [rows] = await connection.execute(query, values);
