815dde539b53cd14b43c0c60524997fd199b80eb
이승훈 <seunghun@iseunghun-ui-MacBookPro.local>
2023-05-19 16:34:26 +0900

    주석 추가 및 DB설정 lambda config로 변경

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
 import mysql from 'mysql2/promise';
-import * as database from "./config/database.json";
+// import * as database from "./config/database.json";
 import * as util from "./util";
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     console.log("Event", event);
+
+    let alias: string = 'dev';
+    let tableName: string = 'users_test';
+    if(event.requestContext.path.includes('/prod/') || event.requestContext.path.includes('/live/')) {
+        alias = 'prod';
+        tableName = 'users';
+    };
 
     let response: APIGatewayProxyResultV2 = {
         statusCode: 200,
@@ -15,7 +22,7 @@ export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<API
         userInfo: {}
     };
 
-    let tableName: string = 'users_test';
+    // body가 입력되지 않았을 경우에는 오류 전달.
     if (!event.body) {
         console.log("Not exist body");
         response.statusCode = 400;
@@ -24,20 +31,22 @@ export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<API
         return response;
     }
 
-    const { body, headers, queryStringParameters } = event;
+    const { body } = event;
     const requestBody = JSON.parse(body);
     console.log("requestBody", requestBody);
 
     let connection;
     try {
+        /** MySQL 연결 **/
         connection = await mysql.createConnection({
-            host: database.host,
-            user: database.user,
-            password: database.password,
-            port: database.port,
-            database: database.database
+            host: process.env[`${alias.toUpperCase()}_DB_HOST`],
+            user: process.env[`${alias.toUpperCase()}_DB_USER`],
+            password: process.env[`${alias.toUpperCase()}_DB_PASSWORD`],
+            port: 3306,
+            database: process.env[`${alias.toUpperCase()}_DB_NAME`]
         });
 
+        /** body의 id값으로 table 조회 후 값이 존재여부 확인 **/
         const query: string = `SELECT COUNT(*) AS count FROM ${tableName} WHERE id = ?`;
         const values: any[] = [requestBody.id];
 
@@ -45,9 +54,10 @@ export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<API
 
         console.log("results", results);
         const count: number = results[0]?.count || 0;
-
         const currentTime: string = new Date().toISOString();
+
         if (count >= 1) {
+            /** 만약 id가 존재한다면 updateTime을 renew **/
             const updateQuery: string = `UPDATE ${tableName} SET updateTime = '${currentTime}' WHERE id = ?`;
             await util.queryMySQL(connection, updateQuery, values);
 
@@ -56,6 +66,7 @@ export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<API
             responseBody.userInfo = results[0];
             response.body = JSON.stringify(responseBody);
         } else {
+            /** 만약 id가 존재하지 않는다면 body.id의 값으로 새로운 데이터 생성 후 값을 전달 **/
             const columns: string = Object.keys(requestBody).join(', ');
             const valuePlaceholders: string = Object.values(requestBody).map(() => '?').join(', ');
             const insertQuery: string = `INSERT INTO ${tableName} (${columns}, createTime, updateTime) VALUES (${valuePlaceholders}, '${currentTime}', '${currentTime}')`;
